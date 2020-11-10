afd9cae990b9f395e1c1f2946bb056fc49c0ab6c
zulpikar321 <zulpikarzulay@gmail.com>
2020-11-10 09:27:49 -0800

    windows

--- src/com/Hello.java
+++ src/com/Hello.java
@@ -3,7 +3,7 @@ package com;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Nice");
-        // Mac user and windo
+        // Mac user and windows are happy
 
 
     }
